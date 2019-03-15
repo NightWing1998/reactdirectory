c2e873938e0ee377e8af80fa0703baeabea449b2
NightWing1998 <dhruvil.s@somaiya.edu>
2019-03-15 17:54:52 +0530

    pushing semi final

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import AppBar from './components/AppBar';
 import { createMuiTheme,MuiThemeProvider } from '@material-ui/core/styles';
 import { Button } from '@material-ui/core';
+import { List, ListItem,Typography,IconButton,ListItemSecondaryAction } from '@material-ui/core';
+import { Delete } from '@material-ui/icons';
 
 const AppBartheme = createMuiTheme({
 	palette : {
@@ -29,7 +31,83 @@ const AppBartheme = createMuiTheme({
 	},
 });
 
+var typography = {
+	width : '33%',
+	margin : '0',
+	float : 'left',
+	opacity : '0.8',
+	textAlign : 'center',
+	letterSpacing : '1px'
+},
+iconButton = {
+	color : 'crimson',
+	opacity : '0.8'
+},header = {
+	width : '33%',
+	margin : '0',
+	float : 'left',
+	textAlign : 'center',
+	letterSpacing : '1px'
+}
+
+const mainTheme = createMuiTheme({
+	palette : {
+		primary : {
+			light : '#555555',
+			dark : '#111111',
+			main : '#333333',
+			contrastText : '#ffffff'
+		},
+		secondary : {
+			light : '#ffffff',
+			dark : '#dddddd',
+			main : '#eeeeee',
+			contrastText : '#111111'
+		},
+		error : {
+			light : '#c00000',
+			dark : '#500000',
+			main : '#0a0000',
+			contrastText : '#00ffff'
+		}
+	},
+	typography: {
+		useNextVariants: true,
+		width : '33%',
+		margin : '0',
+		float : 'left',
+		opacity : '0.8'
+	},
+	iconButton : {
+		color : 'crimson',
+		opacity : '0.8'
+	}
+});
+
 class App extends Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			contacts : [{
+				name : 'Dhruvil',
+				number : '8850392965'
+			},{
+				name : 'Rahil',
+				number : '9869225071'
+			},{
+				name : 'Khushali',
+				number : '9699580280'
+			}],
+		}
+	}
+
+	handleDelete = (number) => {
+		console.log(number.number,this.state);
+		this.setState(({ contacts }) => ({
+			contacts: contacts.filter(ex => ex.number !== number.number)
+		}))
+	}
+	
 	render() {
 		return (
 			<div>
@@ -38,10 +116,30 @@ class App extends Component {
 						<AppBar />
 					</MuiThemeProvider>
 				</header>
-				<Button >ADD</Button>
+				<main>
+					<MuiThemeProvider theme={mainTheme}>
+							<Button >ADD</Button><br/>
+							<Typography variant = 'h6' style={header}>NAME</Typography>
+							<Typography variant = 'h6' style={header}>NUMBER</Typography>
+							<List>
+								{ this.state.contacts.map( ({name,number}) => 
+									<ListItem key={number}>
+										<Typography variant = 'h6' style={typography}>{name}</Typography>
+										<Typography variant = 'h6'style={typography}>{number}</Typography>
+										<ListItemSecondaryAction>
+											<IconButton style={iconButton} onClick={()=>this.handleDelete({number})} >
+												<Delete />
+											</IconButton>	
+										</ListItemSecondaryAction>	
+									</ListItem>
+								)}
+							</List>
+					</MuiThemeProvider>
+				</main>
 			</div>
 		);
 	}
+
 }
 
 export default App;
--- src/components/contact.js
+++ src/components/contact.js
@@ -1,4 +1,28 @@
 import React from 'react';
+import {Typography,IconButton} from '@material-ui/core';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import { Delete } from '@material-ui/icons';
+
+const style = ({
+	root: {
+		width : '100%',
+		height : 'auto',
+		alignItems : 'center',
+		justifyContent : 'space-between',
+	},
+	typography : {
+		width : '33%',
+		margin : '0',
+		float : 'left',
+		opacity : '0.8',
+		textAlign : 'center'
+	},
+	iconButton : {
+		color : 'crimson',
+		opacity : '0.8'
+	}
+});
 
 class ContactTile extends React.Component{
 	constructor(props){
@@ -6,15 +30,27 @@ class ContactTile extends React.Component{
 		this.state = {
 			name : props.name,
 			number : props.number,
-			mail : props.mail
-		}
+			delete : props.buttonClick
+		};
+		this.classes = props.classes;
 	}
 
 	render(){
+		const {classes} = this.props;
 		return (
-			<div className="ContactCard"></div>
+			<div className={classes.root}>
+				<Typography variant = 'h6' className={classes.typography}>{this.state.name}</Typography>
+				<Typography variant = 'h6' className={classes.typography}>{this.state.number}</Typography>
+				<IconButton className={classes.iconButton} onClick={()=>this.state.delete(this.state.number)} >
+					<Delete />
+				</IconButton>	
+			</div>
 		);
 	};
 };
 
-export default ContactTile;
+ContactTile.propTypes = {
+	classes : PropTypes.object.isRequired
+}
+
+export default withStyles(style)(ContactTile);
